8d8a7a7eed0f181fecaa41c2e47150292a6d9622
Alexander Matiazh <a.matiazh@gmail.com>
2024-02-19 14:50:40 +0300

    Merge remote-tracking branch 'origin/master' добавлен декоратор измерения скорости сортировки

--- Insert Sort.py
+++ Insert Sort.py
@@ -1,12 +1,34 @@
+import time
+import random
+
+def time_counter(func):
+    def wraper(args):
+        now = time.perf_counter()
+        x = func(args)
+        newnow = time.perf_counter()
+        print(f"Функция  сработала за {newnow - now} секунды")
+        return x
+    return wraper
+
+
 s = [1000000, 1000000, 59, 29, 32, 23, 1, -1, 9, 19, 17, 31, 23, 46, 92, -1, -1, -1, -1, -1, 1000000]
+
+
 # s = [3, 5, 1, 2, 4]
+@time_counter
+def insert_sort(s):
+    for i in range(1, len(s)):
+        x = s[i]
+        j = i - 1
+        while j >= 0 and s[j] > x:
+            s[j + 1] = s[j]
+            j -= 1
+        s[j + 1] = x
+    return s
 
-for i in range(1, len(s)):
-    x = s[i]
-    j = i - 1
-    while j >= 0 and s[j] > x:
-        s[j + 1] = s[j]
-        j -= 1
-    s[j + 1] = x
 
-print(s)
+#s = random.randint(1, 1000000,)
+# s = [3, 5, 1, 2, 4]
+s = [random.randint(-1000000, 1000000) for _ in range(10000)]
+#print(s)
+print(insert_sort(s))
