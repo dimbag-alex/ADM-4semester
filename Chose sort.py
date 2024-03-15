0a20ef9011ff01072e16250e3184b69d3f42e1a0
amatiazh <amatiazh@ctrlops.ru>
2024-03-15 20:10:41 +0300

    Added ChooseSort code and ShellSort description

--- Chose sort.py
+++ Chose sort.py
@@ -1,2 +1,18 @@
 # сортировка выбором:
-# на каждом шаге выбираем минимум из оставшегося массива и ставим на  массивом
+# на каждом шаге выбираем минимум из оставшегося массива и ставим на массивом
+import random
+def choose_sort(m: list) -> list:
+    for c in range(len(m)):
+        curmin_index = c
+        for elem_index in range(c, len(m)):
+            if m[elem_index] < m[curmin_index]:
+                curmin_index = elem_index
+        m[curmin_index], m[c] = m[c], m[curmin_index]
+    return m
+
+
+to_be_sorted = [-1, 0, 5, 6, -3, 5, 9, 111]
+print(choose_sort(to_be_sorted))
+
+real_check = [random.randint(-1000,1000) for _ in range(1000)]
+print(choose_sort(real_check) == sorted(real_check)
